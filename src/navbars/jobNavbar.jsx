456a3b1e4987d245a6b08e137d51ea3c84155834
Dhiraj-4 <dhirajdipaklondhe4@gmail.com>
2025-09-28 23:13:57 +0530

    missing part from last commit, it's about jobPostStore

--- src/navbars/jobNavbar.jsx
+++ src/navbars/jobNavbar.jsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useState } from "react";
 import { Briefcase } from "lucide-react";
 
 export default function JobNavbar({ activeTab, setActiveTab }) {
--- src/store/jobPostStore.js
+++ src/store/jobPostStore.js
@@ -1,5 +1,15 @@
 import { create } from "zustand";
 
 export const useJobStore = create((set,get) => ({
-    
+    role: "",
+    setRole: (role) => set({ role }),
+
+    companyName: "indie",
+    setCompanyName: (name) => set({ companyName: name }),
+
+    applyLink: "",
+    setApplyLink: (link) => set({ applyLink: link }),
+
+    text: "",
+    setText: (text) => set({ text })
 }));
