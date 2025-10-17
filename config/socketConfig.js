f2a8c7a1f283eb1856c2a56e4b2cb67f95824e90
Dhiraj Londhe <dhirajdipaklondhe4@gmail.com>
2025-10-17 23:00:48 +0530

    changed socketConfig.js, enabled auto connect and imported SOCKET_URL and will use that instead of BACKEND_URL

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
-import { BACKEND_URL } from "./envConfig.js";
-
-const SOCKET_URL = BACKEND_URL;
+import { SOCKET_URL } from "./envConfig.js";
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
-  autoConnect: false, // we’ll connect manually after login
+  autoConnect: true, // we’ll connect manually after login
 });
